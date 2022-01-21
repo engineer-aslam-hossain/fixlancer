f6431b37bc374adab7ccb6f9384ff424b5650eb5
engineer-aslam-hossain <engineer.aslamhossain@gmail.com>
2022-01-22 00:19:34 +0600

    some changes are made

--- components/NewsCard/NewsCard.js
+++ components/NewsCard/NewsCard.js
@@ -6,7 +6,13 @@ const NewsCard = ({ news, indx }) => {
   return (
     <Col lg="6" className={classes.news_col}>
       <div className={classes.news_card}>
-        <img className={classes.newsImg} src={news.img} alt="newscard_img" />
+        <Image
+          className={classes.newsImg}
+          width={200}
+          height={100}
+          src={news.img}
+          alt="newscard_img"
+        />
         <h5>{news.title}</h5>
         <p>{news.description}</p>
       </div>
